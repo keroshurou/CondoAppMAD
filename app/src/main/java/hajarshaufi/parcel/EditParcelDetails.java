6df855cb93c7f10c28d55a2e02090ae392bb0423
keroshurou <hjr.raven@gmail.com>
2022-12-27 12:39:03 +0800

    3.1

--- app/src/main/java/hajarshaufi/parcel/EditParcelDetails.java
+++ app/src/main/java/hajarshaufi/parcel/EditParcelDetails.java
@@ -2,19 +2,39 @@ package hajarshaufi.parcel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class EditParcelDetails extends AppCompatActivity {
 
     ImageButton btnBack;
-    EditText editManagementID, editCollectorName, editParcelUnit, editExpressBrand, editTrackingNumber, editDeliveredDate,
-    editCollectStatus, editCollectedDate;
+    EditText edtUpManagementID, edtUpCollectorName, edtUpUnitNumber, edtUpTrackingNumber, edtUpExpressBrand,
+            edtUpDeliveredDate, edtUpCollectedStatus, edtUpCollectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_parcel_details);
+
+        btnBack = findViewById(R.id.btnBack);
+        edtUpManagementID = findViewById(R.id.edtUpManagementID);
+        edtUpCollectorName = findViewById(R.id.edtUpCollectorName);
+        edtUpUnitNumber = findViewById(R.id.edtUpUnitNumber);
+        edtUpTrackingNumber = findViewById(R.id.edtUpTrackingNumber);
+        edtUpExpressBrand = findViewById(R.id.edtUpExpressBrand);
+        edtUpDeliveredDate = findViewById(R.id.edtUpDeliveredDate);
+        edtUpCollectedStatus = findViewById(R.id.edtUpCollectedStatus);
+        edtUpCollectedDate = findViewById(R.id.edtUpCollectedDate);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBack = new Intent(EditParcelDetails.this, ParcelListView.class);
+                startActivity(intentBack);
+            }
+        });
     }
 }
