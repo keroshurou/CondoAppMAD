192ceaa384e8c1b5802f71906625d71734a09a93
keroshurou <hjr.raven@gmail.com>
2023-01-11 10:37:49 +0800

    bug

--- app/src/main/java/hajarshaufi/parcel/FacilityBookingMenu.java
+++ app/src/main/java/hajarshaufi/parcel/FacilityBookingMenu.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FacilityBookingMenu extends AppCompatActivity {
 
     Button btnBooking, btnViewBooking;
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,16 @@ public class FacilityBookingMenu extends AppCompatActivity {
                 startActivity(intentAdd);
             }
         });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentAdd = new Intent(FacilityBookingMenu.this,
+                        MainActivity.class);
+                startActivity(intentAdd);
+            }
+        });
+
 
     }
 }
