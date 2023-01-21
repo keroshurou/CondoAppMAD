cc22ee58edf349635782e4c4c3226d279f4935ff
keroshurou <hjr.raven@gmail.com>
2023-01-21 15:52:31 +0800

    complete 2.0

--- app/src/main/java/hajarshaufi/parcel/StaffHomeScreenActivity.java
+++ app/src/main/java/hajarshaufi/parcel/StaffHomeScreenActivity.java
@@ -1,18 +1,28 @@
 package hajarshaufi.parcel;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 import hajarshaufi.parcel.databinding.ActivityStaffHomeScreenBinding;
 
 public class StaffHomeScreenActivity extends AppCompatActivity {
 
     ActivityStaffHomeScreenBinding binding;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,7 @@ public class StaffHomeScreenActivity extends AppCompatActivity {
         binding = ActivityStaffHomeScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+        /*binding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -44,7 +54,7 @@ public class StaffHomeScreenActivity extends AppCompatActivity {
                 });
                 alertDialog.show();
             }
-        });
+        });*/
 
         binding.staffParcel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +88,60 @@ public class StaffHomeScreenActivity extends AppCompatActivity {
         //startActivity(i);
         //}
         // });
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.nav_open,
+                R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navigationView = findViewById(R.id.nav_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()){
+                    /*case R.id.nav_notification:
+                        intent = new Intent(getApplicationContext(), NotificationSlider.class);
+                        startActivity(intent);
+                        return true;*/
+
+                    case R.id.nav_logout:
+
+                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(StaffHomeScreenActivity.this);
+                        alertDialog.setTitle("Exit App");
+                        alertDialog.setMessage("Do you want to exit");
+                        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(StaffHomeScreenActivity.this, LogoutActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                        alertDialog.show();
+
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     @Override
@@ -100,5 +164,13 @@ public class StaffHomeScreenActivity extends AppCompatActivity {
             }
         });
         alertDialog.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
